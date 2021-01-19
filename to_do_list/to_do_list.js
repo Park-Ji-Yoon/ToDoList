da64b64db1358dfe8ae3aa3855034832bffcf3b6
Park-Ji-Yoon <55547842+Park-Ji-Yoon@users.noreply.github.com>
2021-01-19 19:30:39 +0900

    first commit

---
+++ to_do_list/to_do_list.js
@@ -0,0 +1,22 @@
+loadEvents();
+
+console.log("dd");
+function loadEvents(){
+  document.querySelector('form').addEventListener('submit', submit);
+}
+
+function submit(e){
+  e.preventDefault();
+  let input = document.querySelector('input');
+  if(input.value != '')
+    addTask(input.value);
+  input.value = '';
+}
+
+function addTask(task){
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
+  ul.appendChild(li);
+  document.querySelector('.board').style.display = 'block';
+}

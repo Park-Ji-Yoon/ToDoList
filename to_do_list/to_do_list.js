c03f45efb8a6bd81160d6673f399d8eae3fbde00
Park-Ji-Yoon <55547842+Park-Ji-Yoon@users.noreply.github.com>
2021-02-09 21:54:42 +0900

    기능 완성, 디자인 디테일 미완성

--- to_do_list/to_do_list.js
+++ to_do_list/to_do_list.js
@@ -1,37 +1,47 @@
-loadEvents();
-var row = 0;
+loadToDoList(); // 메인 함수 로딩
+var item = 0; // to do list의 개수를 0으로 둠
 
-function loadEvents(){
-  document.querySelector('form').addEventListener('submit', submit);
-  document.querySelector('button').addEventListener('del', removeTask);
+// 메인 함수
+function loadToDoList() {
+    document.querySelector('form').addEventListener('submit', submitToDoList); // form 태그 내에서 submit 발생 시 submitToDoList 함수 실행
+    document.querySelector('ul').addEventListener('click', removeToDoList); // ul 태그 내에서 click 발생 시 removeToDoList 함수 실행
 }
 
-function submit(e){
-  e.preventDefault();
-  let input = document.querySelector('input');
-  if(input.value != '')
-    addTask(input.value);
-  input.value = '';
+// to do list 추가 가능한지 확인하는 함수
+function submitToDoList(event) {
+    event.preventDefault(); // 새로고침 방지 (submit 누르면 보통 새로고침 되는데 그것을 막아줌)
+    let task = document.querySelector('input'); // input 태그에서 입력된 갑을 받아 task 변수에 초기화
+    if (task.value != '') {
+        addToDoList(task.value);
+    }
+    task.value = ''; // task 변수를 비워줌
 }
 
-// function addTask(task){
-//   let ul = document.querySelector('ul');
-//   let li = document.createElement('li');
-//   li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
-//   ul.appendChild(li);
-//   document.querySelector('.board').style.display = 'block';
-// }
-function addTask(task){
-  let ul = document.querySelector('ul');
-  let li = document.createElement('li');
-  li.style = "list-style:none;";
-  li.innerHTML = `<button type="del"; onclick="removeTask(" + row + ")">×</button><label>${task}</label>`;
-  li.setAttribute("id", "Remove");
-  ul.appendChild(li);
-  document.querySelector('.board').style.display = 'block';
-  row += 1
+// to do list 추가하는 함수
+function addToDoList(task) {
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerHTML = `
+    	&nbsp&nbsp&nbsp<input type="checkbox">&nbsp&nbsp&nbsp
+    	<label>
+    		${task}
+    	</label>
+    	<span class="delete"; style="float: right; margin-right: 30px; font-family: 'Raleway', sans-serif; font-weight: 700; font-size: 22px; color: #007b91;">
+    		X
+    	</span>`;
+    ul.appendChild(li);
+    document.querySelector('.to_do_lists').style.display = 'block';
+    item += 1
 }
 
-function removeTask(row){
-  console.log(row)
+// to do list 삭제하는 함수
+function removeToDoList(event) {
+    if (event.target.className == 'delete') {
+        let remove = event.target.parentNode;
+        let parentNode = remove.parentNode;
+        parentNode.removeChild(remove);
+    } else {
+        // tickTask(event);
+        console.log("dfd");
+    }
 }
